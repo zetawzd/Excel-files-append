94352f2dcd39b61953805b91423e93f807d46b2d
zetawzd <tuananh121291@gmail.com>
2017-06-14 15:23:22 +0700

    Update merge.py
    Them code do phai bam lai vao cmd sau khi select folder

--- merge.py
+++ merge.py
@@ -2,13 +2,35 @@ import os
 import pandas as pd
 from tkinter import filedialog
 import tkinter
+import win32com.client
 
-# chon folder bang GUI
+'''
+# doi current working directory
+folder='D:\Python\Pewfolder'
+os.chdir(folder)
+
+# go input folder
+found=False
+while not found:
+	folder=input('Enter path to files: ')
+	if not os.path.isdir(folder):
+		print("Go sai path cmnr, nhap lai de.")
+	else:
+		print("Nhap link dung cmnr, de day lam tiep :)")
+		found=True 
+		os.chdir(folder)
+'''
+
+# select current directory bang graphic user interface (chuot)
 root=tkinter.Tk()
 root.destroy()
 
 folder=filedialog.askdirectory()
 os.chdir(folder)
+
+# focus lai vao cmd, do phai bam chuot
+shell=win32com.client.Dispatch("WScript.Shell")
+shell.SendKeys('%{TAB}')
 
 # tao dataframe trong
 all_data=pd.DataFrame()
@@ -28,4 +50,4 @@ side=all_data.drop(ds,axis=1)
 all_data=pd.concat([main,side],axis=1)
 
 # xuat file excel
-all_data.to_excel('master.xlsx')
+all_data.to_excel(input('Chang hay tien sinh muon dat ten file la gi: ')+'.xlsx')
